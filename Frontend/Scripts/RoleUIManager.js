5a1fce36f8c5d21c8ddc5953358f54ba589c4be6
SamuelRiveira <samuelriveiraescudero@gmail.com>
2025-05-14 22:19:43 +0100

    Quitado boton innecesario

--- Frontend/Scripts/RoleUIManager.js
+++ Frontend/Scripts/RoleUIManager.js
@@ -692,13 +692,6 @@ class RoleUIManager {
                                 <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
                             </svg>
                         </button>
-                        <button class="btn-action btn-cancel">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <circle cx="12" cy="12" r="10"></circle>
-                                <line x1="15" y1="9" x2="9" y2="15"></line>
-                                <line x1="9" y1="9" x2="15" y2="15"></line>
-                            </svg>
-                        </button>
                     </div>
                 </div>
             </div>

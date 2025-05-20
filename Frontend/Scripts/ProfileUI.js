24780bd6a1622568a7262bd37d68b027642fe325
SamuelRiveira <samuelriveiraescudero@gmail.com>
2025-05-20 18:30:11 +0100

    Eliminado edit-icon de la imagen

--- Frontend/Scripts/ProfileUI.js
+++ Frontend/Scripts/ProfileUI.js
@@ -92,12 +92,6 @@ class ProfileUI {
                 <div class="profile-sidebar">
                     <div class="profile-photo">
                         <img src="${this.profileData.photo}" alt="Foto de perfil">
-                        <div class="edit-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                            </svg>
-                        </div>
                     </div>
                     <div class="profile-name">
                         <h2>${this.profileData.name}</h2>

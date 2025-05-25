1bdabbd8a6fd5062124f73ddf7cc506a977d8935
SamuelRiveira <samuelriveiraescudero@gmail.com>
2025-05-25 18:48:28 +0100

    Añadidos últimos datos necesarios en Administración de Citas

--- Frontend/Scripts/RoleUIManager.js
+++ Frontend/Scripts/RoleUIManager.js
@@ -982,6 +982,9 @@ class RoleUIManager {
         const day = appointmentDate.getDate();
         const month = AppointmentDataManager.getMonthName(appointmentDate.getMonth());
         const time = AppointmentDataManager.formatTime(appointmentDate);
+        const idUsuario = await API.obtenerUsuarioIdPorMascotaId(appointment.id_mascota);
+        const usuarioResponse = await API.obtenerPerfilUsuarioId(idUsuario.data.id_usuario);
+        const usuario = usuarioResponse.success ? usuarioResponse.data : null;
         
         // Obtener datos de servicio y mascota en paralelo
         const [servicioResponse, mascotaResponse] = await Promise.all([
@@ -1066,7 +1069,7 @@ class RoleUIManager {
                             <circle cx="12" cy="7" r="4"></circle>
                         </svg>
                     </div>
-                    <span class="owner-name">${'TODO: nombre del propietario'}</span>
+                    <span class="owner-name">${usuario?.nombre || 'Desconocido'}</span>
                 </div>
                 <div class="appointment-vet">
                     <div class="vet-icon">
@@ -1074,7 +1077,7 @@ class RoleUIManager {
                             <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.908.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"></path>
                         </svg>
                     </div>
-                    <span class="vet-name">${'TODO: nombre del veterinario'}</span>
+                    <span class="vet-name">${usuario?.direccion || 'Sin dirección asignada'}</span>
                 </div>
                 <div class="appointment-status">
                     <span class="status-badge ${statusLabel.class}">${statusLabel.text}</span>
